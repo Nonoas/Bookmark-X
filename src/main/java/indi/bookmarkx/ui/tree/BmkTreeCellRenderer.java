08e3f414708da569fe1d0da01582c777cf217c70
Admin <nonoaswy@163.com>
2024-05-02 05:26:25 +0800

    fix the difference of background color between the icon and text in the hover state

--- src/main/java/indi/bookmarkx/ui/tree/BmkTreeCellRenderer.java
+++ src/main/java/indi/bookmarkx/ui/tree/BmkTreeCellRenderer.java
@@ -3,10 +3,10 @@ package indi.bookmarkx.ui.tree;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.util.ui.UIUtil;
 import indi.bookmarkx.common.BaseColors;
 
 import javax.swing.Icon;
-import javax.swing.JComponent;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.Color;
@@ -22,6 +22,11 @@ import java.awt.Graphics;
  * @since 1.2.1
  */
 public class BmkTreeCellRenderer extends DefaultTreeCellRenderer {
+
+    public BmkTreeCellRenderer() {
+//        ReflectionUtil.setField(DefaultTreeCellRenderer.class, this, Boolean.TYPE, "fillBackground", false);
+    }
+
     @Override
     public Component getTreeCellRendererComponent(JTree tree,
                                                   Object value,
@@ -30,9 +35,10 @@ public class BmkTreeCellRenderer extends DefaultTreeCellRenderer {
                                                   boolean isLeaf,
                                                   int row,
                                                   boolean hasFocus) {
-        JComponent component = (JComponent) super.getTreeCellRendererComponent(tree, value, selected, expanded, isLeaf, row, hasFocus);
-        component.setOpaque(true);
-        component.setBackground(Color.RED);
+
+        setColor(selected, hasFocus);
+
+        super.getTreeCellRendererComponent(tree, value, selected, expanded, isLeaf, row, hasFocus);
 
         BookmarkTreeNode node = (BookmarkTreeNode) value;
         Icon icon = null;
@@ -45,7 +51,18 @@ public class BmkTreeCellRenderer extends DefaultTreeCellRenderer {
                     : AllIcons.Nodes.Folder;
         }
         setIcon(icon);
-        return component;
+        return this;
+    }
+
+    /**
+     * This method fix the difference of background color between the icon and text in the hover state
+     */
+    private void setColor(boolean selected, boolean hasFocus) {
+        this.setBorderSelectionColor(null);
+        this.setBackgroundSelectionColor(null);
+        this.setBackgroundNonSelectionColor(null);
+        this.setBackground(UIUtil.getTreeBackground(selected, hasFocus));
+        this.setForeground(UIUtil.getTreeForeground(selected, hasFocus));
     }
 
     @Override
