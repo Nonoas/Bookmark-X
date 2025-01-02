136668f2380b1f7a0d78698e5c24e78d8f3169ae
Nonoas <1356955215@qq.com>
2025-01-02 15:33:22 +0800

    添加beta版本更新获取

--- src/main/java/indi/bookmarkx/common/Constants.java
+++ src/main/java/indi/bookmarkx/common/Constants.java
@@ -11,4 +11,11 @@ public interface Constants {
      * plugin.xml 文件中的 key 名
      */
     String PLUGIN_NAME = "Bookmark-X";
+
+    String PLUGIN_ID = "indi.nonoas.bookmarkx";
+
+    /**
+     * 插件在IDEA插件官网的id
+     */
+    String PLUGIN_MARKET_ID = "22013";
 }
